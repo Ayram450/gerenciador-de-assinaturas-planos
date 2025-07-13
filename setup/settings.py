861ffabacc1131082741d615ad7370ab1bce3e9c
Marya Clara <mclaracosta45@gmail.com>
2025-07-13 02:48:41 -0400

    ajuste de secret info em .env

--- setup/settings.py
+++ setup/settings.py
@@ -128,8 +128,8 @@ EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 EMAIL_HOST = "smtp.gmail.com"
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = "subscrybeflow@gmail.com"
-EMAIL_HOST_PASSWORD = "oknd jeut qget flbm"
+EMAIL_HOST_USER = config("EMAIL_HOST_USER")
+EMAIL_HOST_PASSWORD = config("EMAIL_HOST_PASSWORD")
 DEFAULT_FROM_EMAIL = "SubscrybeFlow"
 
 # Static files (CSS, JavaScript, Images)
